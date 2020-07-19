2a9b4ba9bea4197ddb21462c8d8b29c6190c04ca
Jan Erik Breimo <jebreimo@gmail.com>
2020-07-19 22:18:35 +0200

    Create Yconvert.

---
+++ src/Yconvert/Convert.cpp
@@ -0,0 +1,13 @@
+//****************************************************************************
+// Copyright © 2020 Jan Erik Breimo. All rights reserved.
+// Created by Jan Erik Breimo on 2020-06-12.
+//
+// This file is distributed under the BSD License.
+// License text is included with the source distribution.
+//****************************************************************************
+#include "Yconvert/Convert.hpp"
+
+namespace Yconvert
+{
+
+}
